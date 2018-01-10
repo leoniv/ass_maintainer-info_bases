5be19b946d4b785e3bb1cda8e41854ff1f631a55
Leonid Vlasov <leoniv.vlasov@gmail.com>
2018-01-10 15:43:01 +0700

    docs

--- README.md
+++ README.md
@@ -47,6 +47,70 @@ Or install it yourself as:
 
 ```
 
+### `TmpInfoBase`
+
+#### Convert application xml source to +.cf+ file
+
+```ruby
+require 'ass_maintainer/info_bases/tmp_info_base'
+
+PLATFORM_REQUIRE = '~> 8.3.10.0'
+
+src = File.expand_path('../app.src', __FILE__)
+
+# Do in the block with auto remove infobase
+AssMaintainer::InfoBases::TmpInfoBase.make_rm src,
+  platform_require: PLATFORM_REQUIRE do |ib|
+  ib.db_cfg.dump('tmp/app.cf')
+end
+
+# Or remove infobase manually
+tmp_ib = AssMaintainer::InfoBases::TmpInfoBase
+  .new(src, platform_require: PLATFORM_REQUIRE)
+tmp_ib.make
+tmp_ib.db_cfg.dump('tmp/app.cf')
+tmp_ib.rm!
+```
+
+#### Update application +.cf+ file up to required version from update files +.cfu+
+
+```ruby
+require 'ass_maintainer/info_bases/tmp_info_base'
+
+class Updater < AssMaintainer::InfoBases::TmpInfoBase
+  def update_to(cfu_file, force = false)
+    designer do
+      _UpdateCfg cfu_file do
+        _Force if force
+      end
+    end.run.wait.result.verify!
+  end
+
+  def self.execute(from_cf, cfu_files, cf_file, force = false)
+    make_rm from_cf do |ib|
+      cfu_files.each do |cfu|
+        ib.update_to(cfu, force)
+      end
+      ib.cfg.dump(cfu_file)
+    end
+  end
+end
+
+from_cf = File.join(templates_root, 'vendor', 'app', '0', '1cv8.cf')
+cf_file = 'tmp/app.v3.cf'
+
+templates_root = 'path/to/1c/updates'
+cfu_files = ['1','2','3'].map do |v|
+  File.join(templates_root, 'vendor', 'app', v, '1cv8.cfu')
+end
+
+Updater.configure do |conf|
+  conf.platform_require = '~> 8.3.10.0'
+end
+
+Updater.execute(from_cf, cfu_files, cf_file)
+```
+
 ## Development
 
 After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake test` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
--- lib/ass_maintainer/info_bases/tmp_info_base.rb
+++ lib/ass_maintainer/info_bases/tmp_info_base.rb
@@ -2,28 +2,84 @@ module AssMaintainer
   module InfoBases
     require 'ass_maintainer/info_bases/test_info_base'
     require 'ass_maintainer/info_bases/support'
-    # Temporary infobase.
+    # Temporary infobase. Proper for cases when require make temporary
+    # 1C application do anything and remove after.
+    # Temporary infobase is file infobase always. It makes in temporary
+    # directory.
     # @example Convert application xml source to +.cf+ file
     #   require 'ass_maintainer/info_bases/tmp_info_base'
     #
     #   PLATFORM_REQUIRE = '~> 8.3.10.0'
     #
-    #   # Convert application xml source to .cf file
     #   src = File.expand_path('../app.src', __FILE__)
     #
-    #   include AssMaintainer::InfoBases::TmpInfoBase::Api
-    #
-    #   with_tmp_ib src, platform_require: PLATFORM_REQUIRE do |ib|
+    #   # Do in the block with auto remove infobase
+    #   AssMaintainer::InfoBases::TmpInfoBase.make_rm src,
+    #     platform_require: PLATFORM_REQUIRE do |ib|
     #     ib.db_cfg.dump('tmp/app.cf')
     #   end
     #
-    # @example Generate application +.cf+ file required version from
-    # update files +.cfu+
+    #   # Or remove infobase manually
+    #   tmp_ib = AssMaintainer::InfoBases::TmpInfoBase
+    #     .new(src, platform_require: PLATFORM_REQUIRE)
+    #   tmp_ib.make
+    #   tmp_ib.db_cfg.dump('tmp/app.cf')
+    #   tmp_ib.rm!
     #
+    # @example Update application +.cf+ file up to required version from update files +.cfu+
+    #    require 'ass_maintainer/info_bases/tmp_info_base'
+    #
+    #    class Updater < AssMaintainer::InfoBases::TmpInfoBase
+    #      def update_to(cfu_file, force = false)
+    #        designer do
+    #          _UpdateCfg cfu_file do
+    #            _Force if force
+    #          end
+    #        end.run.wait.result.verify!
+    #      end
+    #
+    #      def self.execute(from_cf, cfu_files, cf_file, force = false)
+    #        make_rm from_cf do |ib|
+    #          cfu_files.each do |cfu|
+    #            ib.update_to(cfu, force)
+    #          end
+    #          ib.cfg.dump(cfu_file)
+    #        end
+    #      end
+    #    end
+    #
+    #    from_cf = File.join(templates_root, 'vendor', 'app', '0', '1cv8.cf')
+    #    cf_file = 'tmp/app.v3.cf'
+    #
+    #    templates_root = 'path/to/1c/updates'
+    #    cfu_files = ['1','2','3'].map do |v|
+    #      File.join(templates_root, 'vendor', 'app', v, '1cv8.cfu')
+    #    end
+    #
+    #    Updater.configure do |conf|
+    #      conf.platform_require = '~> 8.3.10.0'
+    #    end
+    #
+    #    Updater.execute(from_cf, cfu_files, cf_file)
     #
     class TmpInfoBase < AssMaintainer::InfoBases::TestInfoBase
       include Support::TmpPath
+      extend Support::TmpPath
 
+      # Mixin for convenience to use.
+      # @example Convert application xml source to +.cf+ file
+      #   require 'ass_maintainer/info_bases/tmp_info_base'
+      #
+      #   PLATFORM_REQUIRE = '~> 8.3.10.0'
+      #
+      #   # Convert application xml source to .cf file
+      #   src = File.expand_path('../app.src', __FILE__)
+      #
+      #   include AssMaintainer::InfoBases::TmpInfoBase::Api
+      #
+      #   with_tmp_ib src, platform_require: PLATFORM_REQUIRE do |ib|
+      #     ib.db_cfg.dump('tmp/app.cf')
+      #   end
       module Api
         # (see AssMaintainer::InfoBases::TestInfoBase.make_rm)
         def with_tmp_ib(template = nil, **options, &block)
